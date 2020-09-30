1fc7f5b6ee9d9f267a12a261b5b5d9e4036e1016
indika <indikank95@gmail.com>
2020-09-30 19:56:15 +0530

    added register user ui

--- itsSCItime/src/app/app.component.ts
+++ itsSCItime/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './register-user/register-user.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'itsSCItime';
 }
+
+//templateUrl: './app.component.html',
--- itsSCItime/src/app/app.module.ts
+++ itsSCItime/src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule ,  } from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatSelectModule,
     BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
